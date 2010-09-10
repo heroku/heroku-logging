305173a1423bfc47a579ffdc7be1c9413649507c
Orion Henry <orion@heroku.com>
2010-09-09 17:26:39 -0700

    make index do something

--- init.rb
+++ init.rb
@@ -1,7 +1,7 @@
 class Heroku::Client
   def logplex(app_name, options = {})
     url = get("/apps/#{app_name}/logplex", options).to_s
-    puts "DEBUG: #{url}"
+    puts "DEBUG: connecting to #{url}"
     ## all this allows us to read progressive output over https
     uri  = URI.parse(url);
     http = Net::HTTP.new(uri.host, uri.port)
@@ -19,11 +19,11 @@ end
 module Heroku::Command
   class Logplex < BaseWithApp
     def index
-      puts "hello world"
+      ## make this do something other than tail?
+      heroku.logplex(app)
     end
 
     def tail
-      puts "TAIL: #{app}"
       heroku.logplex(app)
     end
   end
