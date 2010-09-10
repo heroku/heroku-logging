3d9c5eeb4f8821d500dfec82f876d12c5ce49f9f
Orion Henry <orion@heroku.com>
2010-09-10 01:52:43 -0700

    Add options to session

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 class Heroku::Client
-  def logplex(app_name, options = {})
-    url = get("/apps/#{app_name}/logplex", options).to_s
-    puts "DEBUG: connecting to #{url}"
-    ## all this allows us to read progressive output over https
+  def read_logplex(app_name, options)
+    query = "?" + options.join("&") unless options.empty?
+    puts "/apps/#{app_name}/logplex#{query}"
+    url = get("/apps/#{app_name}/logplex#{query}").to_s
     uri  = URI.parse(url);
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true if uri.scheme == "https"
@@ -28,7 +28,24 @@ module Heroku::Command
   class Logplex < BaseWithApp
     def index
       ## make this do something other than tail?
-      heroku.logplex(app)
+      options = []
+      until args.empty? do
+        case args.shift
+          when "-t", "--tail"   then options << "tail=1"
+          when "-n", "--num"    then options << "num=#{args.shift.to_i}"
+          when "-p", "--pid"    then options << "pid=#{URI.encode(args.shift)}"
+          when "-s", "--source" then options << "source=#{URI.encode(args.shift)}"
+          end
+      end
+      heroku.read_logplex(app, options)
+    end
+
+    def disable
+      puts heroku.delete("/apps/#{app}/logplex", {}).to_s
+    end
+
+    def init
+      puts heroku.post("/apps/#{app}/logplex", {}).to_s
     end
 
     def tail
