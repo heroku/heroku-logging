a541ebefc8cf4c30b04ef8b0fafea8650a29bb8c
Jacob Vorreuter <jake@heroku.com>
2010-10-08 14:39:28 -0700

    renaming resources

--- init.rb
+++ init.rb
@@ -1,7 +1,7 @@
 class Heroku::Client
-  def read_logplex(app_name, options)
-    query = "?" + options.join("&") unless options.empty?
-    url = get("/apps/#{app_name}/logplex#{query}").to_s
+  def read_logs(app_name, options)
+    query = "&" + options.join("&") unless options.empty?
+    url = get("/apps/#{app_name}/logs?logplex=true#{query}").to_s
     uri  = URI.parse(url);
     http = Net::HTTP.new(uri.host, uri.port)
 
@@ -21,40 +21,30 @@ class Heroku::Client
         end
       end
     rescue Timeout::Error, EOFError
-      abort("\n !    Logplex request timed out")
+      abort("\n !    Request timed out")
     end
   end
 
-  def enable_logplex(app_name)
-    post("/apps/#{app_name}/logplex", {}).to_s
-  end
-
-  def disable_logplex(app_name)
-     delete("/apps/#{app_name}/logplex", {}).to_s
-  end
-
-  def logplex_info(app_name)
-    get("/apps/#{app_name}/logplex/info").to_s.gsub(/^\s*$/, '')
+  def log_info(app_name)
+    get("/apps/#{app_name}/logs/info").to_s.gsub(/^\s*$/, '')
   end
 
   def add_drain(app_name, body)
-    post("/apps/#{app_name}/logplex/drains", body).to_s
+    post("/apps/#{app_name}/logs/drains", body).to_s
   end
 
   def remove_drain(app_name, drain_name)
-    delete("/apps/#{app_name}/logplex/drains/#{drain_name}", {}).to_s
+    delete("/apps/#{app_name}/logs/drains/#{drain_name}", {}).to_s
   end
 end
 
 module Heroku::Command
-  class Logplex < BaseWithApp
-    Help.group("logplex") do |group|
-      group.command "logplex:enable",    "enable logplex service"
-      group.command "logplex:disable",   "disable logplex service"
-      group.command "logplex [options]", "show logplex logs"
-      group.command "logplex --tail",    "realtime tail of logplex logs"
-      group.command "logplex:drain add [options]", "add an instance that will receive log messages"
-      group.command "logplex:drain remove [options]", "remove an instance"
+  class Logging < BaseWithApp
+    Help.group("logs") do |group|
+      group.command "logs [options]", "show logs"
+      group.command "logs --tail",    "realtime tail of logs"
+      group.command "logs:drain add [options]", "add an instance that will receive log messages"
+      group.command "logs:drain remove [options]", "remove an instance"
     end
 
     def index
@@ -67,21 +57,13 @@ module Heroku::Command
           when "-s", "--source" then options << "source=#{URI.encode(args.shift)}"
           end
       end
-      heroku.read_logplex(app, options) do |chk|
+      heroku.read_logs(app, options) do |chk|
         puts chk
       end
     end
 
-    def disable
-      puts heroku.disable_logplex(app)
-    end
-
-    def enable
-      puts heroku.enable_logplex(app)
-    end
-
     def info
-      puts heroku.logplex_info(app)
+      puts heroku.log_info(app)
     end
 
     def drain
@@ -96,7 +78,7 @@ module Heroku::Command
               end
           end
           if name.nil? || host.nil? || port.nil?
-            puts "Usage:\theroku logplex:drain add --name <name> --host <host> --port <port>"
+            puts "Usage:\theroku logs:drain add --name <name> --host <host> --port <port>"
           else
             puts heroku.add_drain(app, "name=#{name}&host=#{host}&port=#{port}")
           end
@@ -110,7 +92,7 @@ module Heroku::Command
                 return
               end
           end
-          puts "Usage:\theroku logplex:drain remove --name <name>"
+          puts "Usage:\theroku logs:drain remove --name <name>"
           return
       end
       puts "Unknown or malformed drain command"
